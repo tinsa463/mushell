888a612c84a3ce38d283ff03a77a0ce54029319d
myamusashi <myamusashi@proton.me>
2025-12-04 12:59:04 +0700

    Services: add new services for Record

--- qmlfmt.sh
+++ qmlfmt.sh
@@ -1,35 +1,22 @@
 #!/usr/bin/env bash
 
-# Fungsi untuk backup koma di posisi tertentu
 backup_commas() {
-    local file="$1"
-    # Replace koma dengan placeholder unik yang tidak akan di-format
-    sed -i.bak-comma \
-        -e 's/,\([[:space:]]*\)$/___COMMA___\1/g' \
-        "$file"
+	local file="$1"
+	sed -i.bak-comma \
+		-e 's/,\([[:space:]]*\)$/___COMMA___\1/g' \
+		"$file"
 }
 
-# Fungsi untuk restore koma
 restore_commas() {
-    local file="$1"
-    sed -i 's/___COMMA___/,/g' "$file"
-    rm -f "${file}.bak-comma"
+	local file="$1"
+	sed -i 's/___COMMA___/,/g' "$file"
+	rm -f "${file}.bak-comma"
 }
 
 find . -name "*.qml" -type f | while read -r file; do
-    echo "Formatting: $file"
-    
-    # Backup koma yang mungkin hilang
-    # backup_commas "$file"
-    
-    # Format dengan qmlfmt
-    qmlfmt -t 4 -i 4 -w -b 250 "$file"
-    
-    # Restore koma
-    # restore_commas "$file"
-    
-    # Fix indentasi if tanpa bracket
-    awk '
+	echo "Formatting: $file"
+	qmlfmt -t 4 -i 4 -w -b 250 "$file"
+	awk '
     /^[[:space:]]*if \(.*\)$/ {
         print
         getline
@@ -43,9 +30,8 @@ find . -name "*.qml" -type f | while read -r file; do
         next
     }
     { print }
-    ' "$file" > "$file.tmp"
-    
-    mv "$file.tmp" "$file"
-    sed -i "s/pragma ComponentBehavior$/pragma ComponentBehavior: Bound/g" "$file"
-done
+    ' "$file" >"$file.tmp"
 
+	mv "$file.tmp" "$file"
+	sed -i "s/pragma ComponentBehavior$/pragma ComponentBehavior: Bound/g" "$file"
+done
